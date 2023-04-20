82f519e28a7cfca51082efac7b4018b3305f7401
Eddie Price <elplist@gmail.com>
2023-04-19 21:54:02 -0700

    volume added
    volume  input added to player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState,useEffect, useRef } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faPlay,faAngleLeft,faAngleRight,faPause } from "@fortawesome/free-solid-svg-icons";
 import {playAudio} from '../util'
 
 const Player = ({audioRef,currentSong, setCurrentSong, isPlaying, setIsPlaying, setSongInfo, songInfo, songs, setSongs}) => {
+    const [volume, setVolume] = useState(60);
     //useref html element
     
     //event handler
@@ -59,6 +60,12 @@ const Player = ({audioRef,currentSong, setCurrentSong, isPlaying, setIsPlaying, 
             }
             playAudio(isPlaying, audioRef)
     }
+
+    useEffect(() => {
+        if (audioRef) {
+          audioRef.current.volume = volume / 100;
+        }
+      }, [volume, audioRef]);
   
 
     return (
@@ -86,6 +93,17 @@ const Player = ({audioRef,currentSong, setCurrentSong, isPlaying, setIsPlaying, 
             icon={faAngleRight}
             onClick={()=> skipTraceHandler('skip-forward')} 
             />
+           </div>
+           <div className="volume-wrapper">
+           <div className="volume">
+      
+      <input type="range" 
+      min={0} 
+      max={100} 
+      value={volume}
+      onChange={(e) => setVolume(e.target.value)}
+      />
+    </div>
            </div>
         </div>
     )
